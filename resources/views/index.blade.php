2143fe652dcbaf85147a3cfa23f2202f117ba546
Maulana Fadilah <maulanafadilah028@gmail.com>
2024-01-23 23:15:29 +0700

    adding google analytics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,6 +18,15 @@
 
     <!-- Tailwind CSS -->
     @vite(['resources/css/app.css','resources/js/app.js'])
+
+    <!-- Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.ga4.measurementId') }}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', "{{ config('services.ga4.measurementId') }}");
+    </script>
     
 </head>
 <body>
